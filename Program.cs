1c57be5546ce50eecbd84673d23f2a8ebe4c63b7
nemi <nemelida@bk.ru>
2023-03-26 07:48:24 +0300

    Создание программы

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿void Print1DArr(string[] arr)
+{
+    Console.Write("[");
+    for(int i=0;i<arr.LongLength-1;i++)
+    {
+   
+        Console.Write($"{arr[i]},");
+    }
+    Console.WriteLine($"{arr[arr.LongLength-1]}]");
+    
+}
+
+string[] SortingArray(string[] mas)
+{
+    string[] resultMas=Array.Empty<string>();
+    for(int i=0;i<mas.Length;i++)
+    {
+        if(mas[i].Length<4)
+        {
+            resultMas=resultMas.Concat(new string[]{mas[i]}).ToArray();
+        }
+    }
+    return resultMas;
+}
+
+Console.WriteLine("Напишите массив со знаками пробела для каждого элемента");
+string arr= Console.ReadLine()??"0";
+string[] masl = arr.Split(' ');
+string[] result=SortingArray(masl);
+Print1DArr(result);
