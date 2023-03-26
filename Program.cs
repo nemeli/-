1c4cf29e60188179563055889912e4c8b5921f06
nemi <nemelida@bk.ru>
2023-03-26 14:49:48 +0300

    Добавленно описание файла программы

--- Program.cs
+++ Program.cs
@@ -10,12 +10,12 @@
     
 }
 
-string[] SortingArray(string[] mas)
+string[] SortingArray(string[] mas, int lenStr)
 {
     string[] resultMas=Array.Empty<string>();
     for(int i=0;i<mas.Length;i++)
     {
-        if(mas[i].Length<4)
+        if(mas[i].Length<lenStr)
         {
             resultMas=resultMas.Concat(new string[]{mas[i]}).ToArray();
         }
@@ -26,5 +26,5 @@ string[] SortingArray(string[] mas)
 Console.WriteLine("Напишите массив со знаками пробела для каждого элемента");
 string arr= Console.ReadLine()??"0";
 string[] masl = arr.Split(' ');
-string[] result=SortingArray(masl);
+string[] result=SortingArray(masl,4);
 Print1DArr(result);
